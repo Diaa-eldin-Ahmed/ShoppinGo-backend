a5c03e46aaf14e7e8b9f8d3578f55af9d5a9c9d5
Diaa-eldin-Ahmed <diaa.aboelela@gmail.com>
2023-07-16 17:45:46 +0300

    authGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,10 +6,28 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from 'src/users/dto/signIn.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,21 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { jwtConstants } from './constants';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
       // secret: process.env.SECRET,
-      secret: 'secret',
+      // secret: 'secret',
+      global: true,
+      secret: jwtConstants.secret,
       // signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService, UsersService],
+  exports: [AuthService],
 })
 export class AuthModule {}
